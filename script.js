9078fd41530c0637b2c2d4bfab3371fa82ceb41a
SteppenHasen <dustasura@gmail.com>
2021-03-11 13:28:48 +0000

    Not ended js-file

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+
+    var yearInTheFuture = years + 2021;
 }
-        
+
+var rangeSlider = function(){
+  var range = getElementById("rate"),
+      value = getElementById("current_range");
+    
+  slider.each(function(){
+
+    value.each(function(){
+      var value = $(this).prev().attr('value');
+      $(this).html(value);
+    })
