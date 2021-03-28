0bf86a65f57aa87ad5ebcf314a2fde0845e885e1
SteppenHasen <dustasura@gmail.com>
2021-03-28 16:47:42 +0000

    Everything is done.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function compute()
     var yearInTheFuture = Number(years) + Number(2021);
 
     document.getElementById('prin').innerHTML = principal
-    document.getElementById('rt').innerHTML = rate
+    document.getElementById('rt').innerHTML = rate + '%'
     document.getElementById('int').innerHTML = interest
     document.getElementById('YITF').innerHTML = yearInTheFuture
 
